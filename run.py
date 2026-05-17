2a4f3898621b4a6b9750260f2554e9cbba92899b
a-code-sri <askme24x7@gmail.com>
2026-05-17 12:23:29 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    print("Launching DriveLegal Futuristic Interface...")
+    os.system(f'"{sys.executable}" -m streamlit run streamlit_app.py')
